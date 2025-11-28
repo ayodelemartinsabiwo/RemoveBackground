77e09cf98fd0308894ac150ac86eb709d1b4818b
ayodelemartinsabiwo <ayodelemartinsabiwo@gmail.com>
2025-11-28 05:26:18 +0100

    Remove test scripts for image creation and performance verification

--- create_test_image.py
+++
@@ -1,8 +0,0 @@
-import os
-from PIL import Image
-
-# Create a test image
-test_image = Image.new('RGB', (300, 300), color='blue')
-test_path = 'test_exe.jpg'
-test_image.save(test_path, 'JPEG')
-print(f"Created test image: {test_path}")
